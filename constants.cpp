439042a43fede438e112ef89d922b3e91631dd80
jakub-figat <jakub.figat@neontri.com>
2026-08-24 20:40:16 +0200

    refactor into Game class

--- constants.cpp
+++ constants.cpp
@@ -5,7 +5,6 @@ constexpr float HEIGHT = 800.0;
 constexpr float MIDDLE = WIDTH / 2;
 constexpr float FLOOR_HEIGHT = 600.0;
 
-
 constexpr float BALL_SIZE = 20.0;
 constexpr sf::Vector2f BALL_START_POSITION {MIDDLE - BALL_SIZE / 2.0f, 200.0};
 
@@ -13,3 +12,5 @@ constexpr float GRAVITY = 400.0;
 constexpr float JUMP_VELOCTIY = -250.0;
 constexpr float PADDLE_SPEED = 400.0;
 constexpr float HIT_FORCE = 1.5;
+
+constexpr unsigned int MAX_SCORE = 5;
